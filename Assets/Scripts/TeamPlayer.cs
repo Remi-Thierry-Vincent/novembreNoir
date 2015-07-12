6a714e40af4967a350213f8327f139f5e1ae599d
powy <powypowypowy@gmail.com>
2015-07-13 02:13:59 +0900

    add scene 3 to provide a basic test scene with platforms.Create ladder prefab. Create teamPLayer behavior, ignoring mouse for movement and using ladders.

--- Assets/Scripts/TeamPlayer.cs
+++ Assets/Scripts/TeamPlayer.cs
@@ -4,25 +4,31 @@ using System;
 
 namespace decembreNoir
 {
-	public class RealPlayer : MonoBehaviour
+	public class TeamPlayer : MonoBehaviour
 	{
 		
 		[HideInInspector]
 		public bool facingRight = true;			// For determining which way the player is currently facing.
 		public float moveForce = 2000f;			// Amount of force added to move the player left and right.
 		public float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
-		public float maxReculeSpeed = 3f;				// The fastest the player can travel in the x axis.
 		
 		public float maxJumpSpeed = 500f;				// The fastest the player can travel in the x axis.
+
 		
+		private int defaultGravity = 8; // the normal gravity when not climbing a ladder
+
+
 		private int nbJump = 0;
 		private Transform groundCheck;			// A position marking where to check if the player is grounded.
-		private bool grounded = false;			// Whether or not the player is grounded.
+		//private bool grounded = false;			// Whether or not the player is grounded.
 		private bool jump = false;			// true if we want to jump
 		
 		private bool jumpCollision = false;
 		private bool jumpKeyDown = false;
-		
+
+
+		private bool canClimb = false;
+
 		public GameObject currentGun;
 		
 		void Awake()
@@ -39,39 +45,13 @@ namespace decembreNoir
 		// update fixed at 50times per sec
 		void FixedUpdate()
 		{
+			Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+		
+			updateClimb (rigidbody);
+
 			if (!jumpCollision)
 			{
-				// Cache the horizontal input.
-				float h = Input.GetAxisRaw("Horizontal");
-				bool recule = false;
-				
-				// If the input is moving the player right and the player is facing left...
-				if (h > 0 && !facingRight)
-					recule = true;
-				// Otherwise if the input is moving the player left and the player is facing right...
-				else if (h < 0 && facingRight)
-					recule = true;
-				
-				// The Speed animator parameter is set to the absolute value of the horizontal input.
-				//anim.SetFloat("Speed", Mathf.Abs(h));
-				
-				// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-				if (h * GetComponent<Rigidbody2D>().velocity.x < (recule?maxReculeSpeed:maxSpeed))
-					// ... add a force to the player.
-					GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
-				
-				
-				// If the player's horizontal velocity is greater than the maxSpeed...
-				if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > (recule ? maxReculeSpeed : maxSpeed))
-					// ... set the player's velocity to the maxSpeed in the x axis.
-					GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x)
-					                                                   * (recule ? maxReculeSpeed : maxSpeed), GetComponent<Rigidbody2D>().velocity.y);
-				
-				if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) == 0 && h ==0)
-					// ... set the player's velocity to the maxSpeed in the x axis.
-					GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
-				
-				
+				updateHorizontalMove (rigidbody);
 			}
 			//
 			if (jump)
@@ -82,7 +62,7 @@ namespace decembreNoir
 				//}
 				//else
 				{
-					GetComponent<Rigidbody2D>().AddForce(Vector2.up *
+					rigidbody.AddForce(Vector2.up *
 					                                     (10000 + 2000 * nbJump - Math.Min(20, GetComponent<Rigidbody2D>().velocity.y) * 500));
 					print("JUMP velocity : " + GetComponent<Rigidbody2D>().velocity.y + " => " +
 					      (10000 + 2000 * nbJump  - Math.Min(20, GetComponent<Rigidbody2D>().velocity.y) * 500));
@@ -100,27 +80,9 @@ namespace decembreNoir
 			// If the jump button is pressed and the player is grounded then the player should jump.
 			//if (Input.GetButtonDown("Jump") && grounded)
 			//	jump = true;
-			
-			//update facing
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Vector3 velocity = (mousePos - transform.position).normalized;
-			if (velocity.x < 0)
-			{
-				if (facingRight)
-				{
-					Flip();
-				}
-				facingRight = false;
-			}
-			else
-			{
-				if (!facingRight)
-				{
-					Flip();
-				}
-				facingRight = true;
-			}
-			
+
+
+
 			if (nbJump < 2 && Input.GetButton("Jump") && !jumpKeyDown)
 			{
 				jump = true;
@@ -133,7 +95,7 @@ namespace decembreNoir
 			}
 		}
 		
-		void Flip()
+		private void Flip()
 		{
 			// Switch the way the player is labelled as facing.
 			facingRight = !facingRight;
@@ -150,9 +112,29 @@ namespace decembreNoir
 			//	currentGun.transform.position = position;
 			//}
 		}
-		
-		public void OnCollisionEnter2D(Collision2D collision)
+
+		void OnTriggerEnter2D(Collider2D collider)  {
+
+			if (collider.tag == "ladder")
+			{
+				Debug.Log("start climb");
+				canClimb = true;
+			}
+		}
+
+		void OnTriggerExit2D(Collider2D collider)  {
+			
+			if (collider.tag == "ladder")
+			{
+				Debug.Log("stop climb");
+				canClimb = false;
+			}
+		}
+
+		void OnCollisionEnter2D(Collision2D collision)
 		{
+
+
 			if (nbJump > 1)
 			{
 				jumpCollision = true;
@@ -163,6 +145,61 @@ namespace decembreNoir
 			{
 				nbJump = 0;
 				jumpCollision = false;
+			}
+
+
+		}
+
+		private void updateHorizontalMove (Rigidbody2D rigidbody) {
+			// Cache the horizontal input.
+			float horizontalInput = Input.GetAxisRaw("Horizontal");
+			
+			if ((facingRight != (horizontalInput > 0)) && (horizontalInput != 0))
+			{
+				Flip();
+			}
+
+			// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+			if (horizontalInput * rigidbody.velocity.x < maxSpeed)
+				// ... add a force to the player.
+				rigidbody.AddForce(Vector2.right * horizontalInput * moveForce);
+			
+			
+			// If the player's horizontal velocity is greater than the maxSpeed...
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			if (Mathf.Abs (rigidbody.velocity.x) > maxSpeed)
+				rigidbody.velocity = new Vector2(maxSpeed * Mathf.Sign(rigidbody.velocity.x),
+				                                 rigidbody.velocity.y);
+			
+			// The Speed animator parameter is set to the absolute value of the horizontal input.
+			//anim.SetFloat("Speed", Mathf.Abs(h));
+			
+			if (horizontalInput == 0)
+				// ... set the player's velocity to the maxSpeed in the x axis.
+				rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+		}
+
+		private void updateClimb (Rigidbody2D rigidbody)
+		{
+			if (canClimb) {
+				rigidbody.gravityScale = 0;
+				
+				float veritacalInput = Input.GetAxisRaw("Vertical");
+				if (veritacalInput * rigidbody.velocity.y < maxSpeed)
+					rigidbody.AddForce(Vector2.up * veritacalInput * moveForce);
+
+				if (Mathf.Abs (rigidbody.velocity.y) > maxSpeed)
+					rigidbody.velocity = new Vector2(rigidbody.velocity.x,
+					                                 maxSpeed * Mathf.Sign(rigidbody.velocity.y));
+				
+				// The Speed animator parameter is set to the absolute value of the horizontal input.
+				//anim.SetFloat("Speed", Mathf.Abs(h));
+				
+				if (veritacalInput == 0)
+				    rigidbody.velocity = new Vector2(rigidbody.velocity.x, 0);
+
+			} else {
+				rigidbody.gravityScale = defaultGravity;
 			}
 		}
 	}
